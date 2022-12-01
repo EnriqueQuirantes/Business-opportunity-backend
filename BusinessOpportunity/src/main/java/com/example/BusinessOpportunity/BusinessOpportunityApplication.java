3b45d34d5811784fbc0b75caf14408d23d94be44
EnriqueQuirantes <enriquequirantes@gmail.com>
2022-12-01 09:44:41 +0100

    first commit

---
+++ BusinessOpportunity/src/main/java/com/example/BusinessOpportunity/BusinessOpportunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.BusinessOpportunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessOpportunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessOpportunityApplication.class, args);
+	}
+
+}
---
+++ BusinessOpportunity/src/test/java/com/example/BusinessOpportunity/BusinessOpportunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BusinessOpportunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessOpportunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
